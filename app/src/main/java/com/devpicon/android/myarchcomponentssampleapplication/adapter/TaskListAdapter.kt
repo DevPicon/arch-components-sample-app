719a286649d36502ee2eb72a69adcebbfc82fda5
Armando Picón <picondev@gmail.com>
2019-04-22 23:02:51 -0400

    fix compatibility

--- app/src/main/java/com/devpicon/android/myarchcomponentssampleapplication/adapter/TaskListAdapter.kt
+++ app/src/main/java/com/devpicon/android/myarchcomponentssampleapplication/adapter/TaskListAdapter.kt
@@ -12,18 +12,18 @@ class TaskListAdapter(context: Context) : RecyclerView.Adapter<TaskViewHolder>()
     private val inflater: LayoutInflater = LayoutInflater.from(context)
     private var tasks: MutableList<Task> = mutableListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TaskViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val itemView = inflater.inflate(R.layout.item_task, parent, false)
         return TaskViewHolder(itemView)
     }
 
     override fun getItemCount(): Int = tasks.size
 
-    override fun onBindViewHolder(holder: TaskViewHolder?, position: Int) {
-        holder?.bind(tasks[position].description)
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(tasks[position].description)
     }
 
-    fun setTasks(tasks : MutableList<Task>){
+    fun setTasks(tasks: MutableList<Task>) {
         this.tasks = tasks
         notifyDataSetChanged()
     }
